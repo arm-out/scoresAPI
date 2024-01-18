b781be5a940172d869f8d64b5ab0b4c7cbb24372
arm-out <arminsuraj@gmail.com>
2024-01-17 17:41:32 -0800

    error handling

--- scrape.js
+++ scrape.js
@@ -7,75 +7,82 @@ const scrapeLogic = async (res, league) => {
 		"https://www.espn.com/nfl/scoreboard/_/week/1/year/2023/seasontype/3";
 
 	const browser = await puppeteer.launch({ headless: "new" });
-	const page = await browser.newPage();
 
-	if (league === "nfl") await page.goto(NFL_SCOREBOARD);
-	else if (league === "nba") await page.goto(NBA_SCOREBOARD);
-	else {
-	}
+	try {
+		const page = await browser.newPage();
 
-	const games = await page.evaluate(() => {
-		const gameCard = document.querySelectorAll("section.gameModules");
-		const dateNow = new Date();
-		// const dateNow = new Date(Date.parse("Sunday, January 14, 2024"));
+		if (league === "nfl") await page.goto(NFL_SCOREBOARD);
+		else if (league === "nba") await page.goto(NBA_SCOREBOARD);
 
-		for (let i = 0; i < gameCard.length; i++) {
-			const date = gameCard[i].querySelector("header h3").innerHTML;
-			const gameDate = new Date(Date.parse(date));
-			if (
-				gameDate.getFullYear() != dateNow.getFullYear() ||
-				gameDate.getMonth() != dateNow.getMonth() ||
-				gameDate.getDate() != dateNow.getDate()
-			) {
-				continue;
+		const games = await page.evaluate(() => {
+			const gameCard = document.querySelectorAll("section.gameModules");
+			const dateNow = new Date();
+			// const dateNow = new Date(Date.parse("Sunday, January 14, 2024"));
+
+			for (let i = 0; i < gameCard.length; i++) {
+				const date = gameCard[i].querySelector("header h3").innerHTML;
+				const gameDate = new Date(Date.parse(date));
+				if (
+					gameDate.getFullYear() != dateNow.getFullYear() ||
+					gameDate.getMonth() != dateNow.getMonth() ||
+					gameDate.getDate() != dateNow.getDate()
+				) {
+					continue;
+				}
+
+				const scores = gameCard[i].querySelectorAll(
+					"div.ScoreboardScoreCell"
+				);
+				const games = Array.from(scores).map((cell) => {
+					const time = cell.querySelector(
+						"div.ScoreCell__Time"
+					).innerHTML;
+					const note =
+						cell.querySelector("div.ScoreboardScoreCell__Note")
+							?.innerHTML || "";
+					const teams = cell.querySelectorAll("li");
+
+					return {
+						time,
+						note,
+						team1: {
+							logo: teams[0].querySelector("img").src,
+							name: teams[0].querySelector(
+								"div.ScoreCell__TeamName"
+							).innerHTML,
+							record: teams[0].querySelector(
+								"span.ScoreboardScoreCell__Record"
+							).innerHTML,
+							score:
+								teams[0].querySelector("div.ScoreCell__Score")
+									?.innerHTML || "",
+						},
+						team2: {
+							logo: teams[1].querySelector("img").src,
+							name: teams[1].querySelector(
+								"div.ScoreCell__TeamName"
+							).innerHTML,
+							record: teams[1].querySelector(
+								"span.ScoreboardScoreCell__Record"
+							).innerHTML,
+							score:
+								teams[1].querySelector("div.ScoreCell__Score")
+									?.innerHTML || "",
+						},
+					};
+				});
+
+				return { games };
 			}
+		});
 
-			const scores = gameCard[i].querySelectorAll(
-				"div.ScoreboardScoreCell"
-			);
-			const games = Array.from(scores).map((cell) => {
-				const time = cell.querySelector(
-					"div.ScoreCell__Time"
-				).innerHTML;
-				const note =
-					cell.querySelector("div.ScoreboardScoreCell__Note")
-						?.innerHTML || "";
-				const teams = cell.querySelectorAll("li");
-
-				return {
-					time,
-					note,
-					team1: {
-						logo: teams[0].querySelector("img").src,
-						name: teams[0].querySelector("div.ScoreCell__TeamName")
-							.innerHTML,
-						record: teams[0].querySelector(
-							"span.ScoreboardScoreCell__Record"
-						).innerHTML,
-						score:
-							teams[0].querySelector("div.ScoreCell__Score")
-								?.innerHTML || "",
-					},
-					team2: {
-						logo: teams[1].querySelector("img").src,
-						name: teams[1].querySelector("div.ScoreCell__TeamName")
-							.innerHTML,
-						record: teams[1].querySelector(
-							"span.ScoreboardScoreCell__Record"
-						).innerHTML,
-						score:
-							teams[1].querySelector("div.ScoreCell__Score")
-								?.innerHTML || "",
-					},
-				};
-			});
-
-			return { games };
-		}
-	});
-
-	browser.close();
-	res.send(games);
+		res.send(games);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send(err);
+	} finally {
+		await browser.close();
+	}
 };
 
 module.exports = { scrapeLogic };
