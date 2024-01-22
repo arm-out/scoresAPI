fc861ab9f6af7037e8a2a4b77572971d7df55744
arm-out <arminsuraj@gmail.com>
2024-01-22 11:20:52 -0800

    nba

---
+++ src/routes/api/[league]/+server.ts
@@ -0,0 +1,50 @@
+import { error, type RequestHandler } from '@sveltejs/kit';
+
+const date = new Date().toISOString().split('T')[0];
+const BASKETBALL = `https://api.sofascore.com/api/v1/sport/basketball/scheduled-events/${date}`;
+
+export const GET: RequestHandler = async ({ fetch, params }) => {
+    const options = {
+        method: 'GET',
+        headers: {
+          authority: 'api.sofascore.com',
+          accept: '*/*',
+          'accept-language': 'en-US,en;q=0.9',
+          'cache-control': 'no-cache',
+          dnt: '1',
+          origin: 'https://www.sofascore.com',
+          pragma: 'no-cache',
+          referer: 'https://www.sofascore.com/',
+          'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+          'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        }
+    };
+
+    const res = await fetch(BASKETBALL, options);
+    const games = await res.json();
+
+    let nba = games.events.filter((game: any) => game.tournament.name === 'NBA');
+    nba = nba.filter((game: any) => new Date(parseInt(game.startTimestamp) * 1000).toLocaleDateString() === new Date().toLocaleDateString());
+    
+    let apiRes = nba.map((game: any) => ({
+        home: game.homeTeam.shortName,
+        away: game.awayTeam.shortName,
+        startTime: game.startTimestamp,
+        status: game.status.type,
+        statusDesc: game.status.description,
+        homeScore: game.homeScore.current ? game.homeScore.current : 0,
+        awayScore: game.awayScore.current ? game.awayScore.current : 0,
+        period: game.lastPeriod ? game.lastPeriod : 0,
+        periodLength: game.time.periodLength ? game.time.periodLength : 0,
+        overtimeLength: game.time.overtimeLength ? game.time.overtimeLength : 0,
+        timePlayed: game.time.played ? game.time.played : 0,
+    }))
+
+
+	return new Response(JSON.stringify(apiRes));
+};
