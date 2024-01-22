a96177804b01890b283837a34c3b1b23fb656ce8
arm-out <arminsuraj@gmail.com>
2024-01-22 15:28:51 -0800

    log date

--- src/routes/api/[league]/+server.ts
+++ src/routes/api/[league]/+server.ts
@@ -31,13 +31,14 @@ export const GET: RequestHandler = async ({ fetch, params }) => {
 
     let filtered = games.events.filter((game: any) => game.tournament.uniqueTournament.name === params.league);
     let today = filtered.filter((game: any) => new Date(parseInt(game.startTimestamp) * 1000).toLocaleDateString() === new Date().toLocaleDateString());
+    console.log(new Date().toLocaleDateString())
 
     // Show previous day games if no games today
     if (today.length == 0) {
         today = filtered
     }
     
-    let apiRes = today.map((game: any) => ({
+    let apiRes = filtered.map((game: any) => ({
         home: game.homeTeam.shortName,
         away: game.awayTeam.shortName,
         startTime: game.startTimestamp,
